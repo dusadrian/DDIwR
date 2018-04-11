205060ffc9665c3d0d8356cf2534f065b8c96925
Adrian Dusa <dusa.adrian@gmail.com>
2018-04-12 01:00:34 +0300

    refactored version, using the package xml2

--- R/getMetadata.R
+++ R/getMetadata.R
@@ -1,9 +1,27 @@
-require(XML)
-getMetadata <- function(xmlpath, OS = "windows", saveFile = FALSE, ...) {
+`getMetadata` <- 
+function(xmlpath, OS = "windows", saveFile = FALSE, ...) {
     
     # TODO: detect DDI version or ask the version through a dedicated argument
     
+    possibleNumeric <- function(x) {
+        # as.character converts everything (especially factors)
+        return(!any(is.na(suppressWarnings(as.numeric(na.omit(as.character(x)))))) & !all(is.na(x)))
+    }
     
+    asNumeric <- function(x) {
+        return(suppressWarnings(as.numeric(as.character(x))))
+    }
+    
+    trimstr <- function(x, side = "both") {
+        pattern <- switch(side,
+            both = paste("^", "[[:space:]]", "+|", "[[:space:]]", "+$", sep = ""),
+            left = paste("^", "[[:space:]]", "+", sep = ""),
+            right = paste("[[:space:]]", "+$", sep = "")
+        )
+        x <- gsub(pattern, "", x)
+        gsub("\\n", "", x)
+    }
+
     other.args <- list(...)
     
     enter <- getEnter(OS)
@@ -13,104 +31,114 @@ getMetadata <- function(xmlpath, OS = "windows", saveFile = FALSE, ...) {
         fromsetupfile <- other.args$fromsetupfile
     }
     
-    indent <- 4
-    if ("indent" %in% names(other.args)) {
-        indent <- other.args$indent
-    }
     
     tp <- treatPath(xmlpath, type = "XML")
     
-    currentdir <- getwd()
-    # if (saveFile) {
-        setwd(tp$completePath)
-    # }
-    
     singlefile <- length(tp$files) == 1
     
-    if (!fromsetupfile) {
+    if (!fromsetupfile & !singlefile) {
         cat("Processing:\n")
     }
     
     for (ff in seq(length(tp$files))) {
-        if (!fromsetupfile) {
+        if (!fromsetupfile & !singlefile) {
             cat(tp$files[ff], "\n")
         }
         
+        xml <- read_xml(file.path(tp$completePath, tp$files[ff]))
         
-        dd <- xmlTreeParse(tp$files[ff])$doc$children$codeBook
+        nms <- xml_name(xml_children(xml))
         
-        
-        #### !!! ####
-        # NEVER use getNodeSet() it's toooooo slooooow!!!
-        # use instead xmlElementsByTagName()
-    
-        dd <- xmlElementsByTagName(dd, "dataDscr")[[1]]
-        dd <- xmlElementsByTagName(dd, "var")
-              
-        xmlVarNames <- as.vector(sapply(dd, xmlGetAttr, "name"))
-        # return(drop(xmlVarNames))
+        dd <- xml_children(xml_children(xml)[[which(nms == "dataDscr")]])
+        xmlVarNames <- xml_attr(dd, "name")
         
         metadata <- vector(mode = "list", length = length(dd))
         names(metadata) <- xmlVarNames
         
-        
         for (i in seq(length(dd))) {
-            
-            varlab <- xmlValue(xmlElementsByTagName(dd[[i]], "labl")[[1]])
+            nms <- xml_name(xml_children(dd[[i]]))
+            varlab <- trimstr(xml_contents(xml_children(dd[[i]])[[which(nms == "labl")]]))
             varlab <- gsub("\"", "'", varlab)
             varlab <- gsub("\\\\", "/", varlab)
-            
-            metadata[[xmlVarNames[i]]]$label <- varlab
-            
-            
-            #vallabs <- unlist(lapply(getNodeSet(dd[[i]], "//labl[@level='category']"), xmlValue))
-            vallabs <- xmlElementsByTagName(dd[[i]], "catgry")
+            varlab <- gsub("<\\!\\[CDATA\\[|\\]\\]>", "", varlab)
+            metadata[[i]]$label <- varlab
             
             missng <- NULL
             
-            if (length(vallabs) > 0) {
-                
-                # metadata$vallab[[xmlVarNames[i]]] <- unlist(lapply(getNodeSet(dd[[i]], "//catValu"), xmlValue))
-                values <- unname(unlist(lapply(vallabs, function(x) {
-                    xmlValue(xmlElementsByTagName(x, "catValu")[[1]][[1]])
-                })))
+            if (any(nms == "invalrng")) {
+                inval <- xml_children(xml_children(dd[[i]])[[which(nms == "invalrng")]])
+                for (j in seq(length(inval))) {
+                    missng <- c(missng, xml_attr(inval[[j]], "VALUE"))
+                }
+            }
+            
+            if (any(nms == "catgry")) {
+                vallabs <- xml_children(dd[[i]])[which(nms == "catgry")]
+                values <- unlist(lapply(vallabs, function(x) {
+                    nms <- xml_name(xml_children(x))
+                    trimstr(xml_contents(xml_children(x)[[which(nms == "catValu")]]))
+                }))
                 values <- gsub("\"", "'", values)
                 values <- gsub("\\\\", "/", values)
                 
-                labl <- lapply(vallabs, function(x) {
-                    xmlValue(xmlElementsByTagName(x, "labl")[[1]][[1]])
-                })
-                
-                haslabel <- unlist(lapply(labl, length)) > 0
-                
-                values <- values[haslabel]
-                labl <- unname(unlist(labl[haslabel]))
-                
-                missng <- unname(unlist(lapply(vallabs, function(x) {
-                    missng <- xmlAttrs(x)
-                    if (is.element("missing", names(missng))) {
-                        if (toupper(missng["missing"]) == "YES") {
-                            return(xmlValue(xmlElementsByTagName(x, "catValu")[[1]][[1]]))
-                        }
+                labl <- unlist(lapply(vallabs, function(x) {
+                    nms <- xml_name(xml_children(x))
+                    
+                    if (any(nms == "labl")) {
+                        trimstr(xml_contents(xml_children(x)[[which(nms == "labl")]]))
                     }
-                })))
+                    else {
+                        return(NA)
+                    }
+                }))
                 
-                if (length(values) > 0) {
-                    metadata[[xmlVarNames[i]]]$values <- values
-                    names(metadata[[xmlVarNames[i]]]$values) <- labl
+                values <- values[!is.na(labl)]
+                labl <- labl[!is.na(labl)]
+                
+                missng <- c(missng, values[unlist(lapply(vallabs, function(x) {
+                    attrs <- xml_attrs(x)
+                    if (any(names(attrs) == "missing")) {
+                        return(grepl("Y", xml_attr(x, "missing")))
+                    }
+                    else {
+                        return(FALSE)
+                    }
+                }))])
+                
+                if (possibleNumeric(values)) {
+                    values <- asNumeric(values)
                 }
+                
+                metadata[[i]]$values <- values
+                names(metadata[[i]]$values) <- labl
+            }
+            
+            if (length(missng) > 0) {
+                if (possibleNumeric(missng)) {
+                    missng <- asNumeric(missng)
+                }
+                metadata[[i]]$missing <- sort(unique(missng))
             }
         }
         
         if (saveFile) {
+            currentdir <- getwd()
+            setwd(tp$completePath)
+            
+            indent <- 4
+            if ("indent" %in% names(other.args)) {
+                indent <- other.args$indent
+            }
+            
             sink(paste(tp$filenames[ff], "R", sep = "."))
             cat("metadata <- list()", enter)
             writeRlist(metadata, OS = OS, indent = indent)
             sink()
+            setwd(currentdir)
         }
     }
     
-    setwd(currentdir)
+    
     if (singlefile) {
         return(invisible(metadata))
     }
