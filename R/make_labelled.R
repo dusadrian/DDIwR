da9e513220fe82e85352c4a70fdfe0d2c989c3d8
Adrian Dusa <dusa.adrian@gmail.com>
2022-05-15 01:23:53 +0300

    values and labels of the same type

--- R/make_labelled.R
+++ R/make_labelled.R
@@ -14,10 +14,37 @@
         na_values <- dataDscr[[i]][["na_values"]]
         na_range <- dataDscr[[i]][["na_range"]]
 
-        v <- unname(unlist(unclass(x[, i])))
-        if (pN[i]) {
-            v <- admisc::asNumeric(v)
+        if (!is.null(labels)) {
+            if (admisc::possibleNumeric(labels) && pN[i]) {
+                labels <- admisc::asNumeric(labels)
+            }
+            else {
+                x <- as.character(x)
+                nms <- names(labels)
+                labels <- as.character(labels)
+                names(labels) <- nms
+                pN[i] <- FALSE
+                na_range <- NULL
+            }
         }
+
+        if (!is.null(na_values)) {
+            if (admisc::possibleNumeric(na_values) & pN[i]) {
+                na_values <- admisc::asNumeric(na_values)
+            }
+            else {
+                na_values <- as.character(na_values)
+            }
+        }
+
+        if (pN[i]) {
+            x[[i]] <- admisc::asNumeric(x[[i]])
+        }
+        else {
+            x[[i]] <- as.character(x[[i]])
+        }
+        
+        v <- unname(unlist(unclass(x[, i])))
         
         if (declared) {
             x[[i]] <- declared::declared(v, labels, na_values, na_range, label)
