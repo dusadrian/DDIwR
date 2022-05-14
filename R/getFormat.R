e46db986c9f908654f90e646baedfb21f8c32c39
Adrian Dusa <dusa.adrian@gmail.com>
2022-05-14 16:39:36 +0300

    fixed for declared

--- R/getFormat.R
+++ R/getFormat.R
@@ -5,17 +5,18 @@
     }
     
     string <- is.character(x)
-    x2 <- as.character(x)
+    x2 <- as.character(declared::undeclare(x, drop = TRUE))
 
     while (TRUE) {
         nofchars <- tryCatch(nchar(x2), error = function(x) return(x))
 
         if (!is.list(nofchars)) break
-        
+
         # if here, tryCatch caught an error, most likely a multibyte character
         error <- unlist(strsplit(nofchars[[1]], split = " "))
+        # remove the offending character
         x2 <- x2[-as.numeric(error[length(error)])]
-        
+        # and repeat the loop until no more problems appear
     }
 
     if (length(nofchars) == 0) {
@@ -24,20 +25,26 @@
 
     maxvarchar <- max(nofchars, na.rm = TRUE)
 
-    if (haven::is.labelled(x)) {
+    if (haven::is.labelled(x) | declared::is.declared(x)) {
         labels <- attr(x, "labels")
-        maxvarchar <- max(maxvarchar, nchar(labels))
-        if (is.character(labels)) string <- TRUE
+        if (is.character(labels)) {
+            string <- TRUE
+            maxvarchar <- max(maxvarchar, nchar(labels))
+        }
     }
 
-    decimals <- FALSE
-    if (is.numeric(x)) {
-        decimals <- any(x - floor(x) > 0)
+    decimals <- 0
+    if (admisc::possibleNumeric(x)) {
+        decimals <- admisc::numdec(x)
     }
 
-    return(sprintf("%s%s%s%d", 
-        ifelse(string, "A", "F"), 
-        maxvarchar, 
-        ifelse(string, "", "."),
-        ifelse(decimals, 2, 0)))
+    return(
+        sprintf(
+            "%s%s%s%d", 
+            ifelse(string, "A", "F"),
+            maxvarchar, 
+            ifelse(string, "", "."),
+            ifelse(string, "", decimals)
+        )
+    )
 }
