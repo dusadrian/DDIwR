4455279ce78af4136ab2659bb5fb39341be7830d
Adrian Dușa <dusa.adrian@gmail.com>
2023-07-11 22:21:02 +0300

    correct tests for getEnter()

--- tests/testthat/test-getEnter.R
+++ tests/testthat/test-getEnter.R
@@ -1,6 +1,8 @@
+current_os <- Sys.info()[["sysname"]]
+
 test_that("getEnter() works", {
-  expect_equal(getEnter("Win"), "\r\n")
-  expect_equal(getEnter("Mac"), "\n")
+  expect_equal(getEnter("Win"), ifelse(current_os == "Windows", "\n", "\r\n"))
+  expect_equal(getEnter("Mac"), ifelse(current_os == "Darwin", "\n", "\r"))
   expect_equal(getEnter("Linux"), "\n")
   expect_error(getEnter("Unknown"))
 })
