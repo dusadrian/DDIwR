8ec02c1e79ebcf9677473f9d43309bcb58b789e0
Adrian Dușa <dusa.adrian@gmail.com>
2024-09-27 17:59:09 +0300

    up minor version

--- R/DDIwR_package.R
+++ R/DDIwR_package.R
@@ -32,8 +32,8 @@
 #' \tabular{ll}{
 #'   Package: \tab DDIwR\cr
 #'   Type: \tab Package\cr
-#'   Version: \tab 0.18.6\cr
-#'   Date: \tab 2024-09-10\cr
+#'   Version: \tab 0.18.7\cr
+#'   Date: \tab 2024-09-27\cr
 #'   License: \tab GPL-v3\cr
 #' }
 #'
