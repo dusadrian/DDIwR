67a76ae51facb41668d443085e6d46c046fff7f0
Adrian Dușa <dusa.adrian@gmail.com>
2024-12-05 13:04:23 +0100

    renamed test

--- tests/testthat/test-getCodebook.R
+++ tests/testthat/test-getCodebook.R
@@ -23,25 +23,25 @@ x <- data.frame(
     E = letters[1:6]
 )
 
-xdfm <- getMetadata(x)
+xdfm <- getCodebook(x)
 
 tmp <- tempdir()
 convert(x, to = file.path(tmp, "x.sav"))
-xsav <- getMetadata(file.path(tmp, "x.sav"))
+xsav <- getCodebook(file.path(tmp, "x.sav"))
 
 convert(x, to = file.path(tmp, "x.xml"))
-xxml <- getMetadata(file.path(tmp, "x.xml"))
+xxml <- getCodebook(file.path(tmp, "x.xml"))
 rxml <- readLines(file.path(tmp, "x.xml"))
 
 convert(x, to = file.path(tmp, "x.dta"))
-xdta <- getMetadata(file.path(tmp, "x.dta"))
+xdta <- getCodebook(file.path(tmp, "x.dta"))
 
 convert(x, to = file.path(tmp, "x.rds"))
 
-xrds <- getMetadata(file.path(tmp, "x.rds"))
+xrds <- getCodebook(file.path(tmp, "x.rds"))
 
 
-test_that("getMetadata() works", {
+test_that("getCodebook() works", {
   expect_true(is.list(xdfm$dataDscr))
   expect_true(is.list(xsav$dataDscr))
   expect_true(is.list(xxml$dataDscr))
@@ -49,38 +49,38 @@ test_that("getMetadata() works", {
   expect_true(is.list(xrds$dataDscr))
 
   # no metadata
-  expect_error(getMetadata(data.frame(A = 1:5)))
+  expect_error(getCodebook(data.frame(A = 1:5)))
 
   # still no metadata, but NULL instead of error
-  expect_equal(getMetadata(data.frame(A = 1:5), error_null = TRUE), NULL)
+  expect_equal(getCodebook(data.frame(A = 1:5), error_null = TRUE), NULL)
 
   # file paths should be characters of length 1
-  expect_error(getMetadata(c("file1", "file2")))
-  expect_error(getMetadata(1))
+  expect_error(getCodebook(c("file1", "file2")))
+  expect_error(getCodebook(1))
 })
 
 test_that("an error is triggered if the XML file is not valid", {
     writeLines(rxml[-which(grepl("<docDscr>", rxml))], file.path(tmp, "x.xml"))
-    expect_error(getMetadata(file.path(tmp, "x.xml")))
-    expect_error(getMetadata(tmp, print_processing = FALSE))
+    expect_error(getCodebook(file.path(tmp, "x.xml")))
+    expect_error(getCodebook(tmp, print_processing = FALSE))
 })
 
 ## by removing xmlns="ddi:codebook:2_5" from the codeBook's attributes
 # test_that("an error is triggered if the XML file does not contain a namespace", {
 #     rxml[6] <- ">"
 #     writeLines(rxml, file.path(tmp, "x.xml"))
-#     expect_error(getMetadata(file.path(tmp, "x.xml")))
+#     expect_error(getCodebook(file.path(tmp, "x.xml")))
 # })
 
 # test_that("range of values changes function of metadata information", {
 #     rxml[46] <- "<range UNITS=\"INT\" min=\"-99\"/>"
 #     writeLines(rxml, file.path(tmp, "x.xml"))
-#     xdfm75 <- getMetadata(file.path(tmp, "x.xml"))
+#     xdfm75 <- getCodebook(file.path(tmp, "x.xml"))
 #     # expect_equal(declared::missing_range(xdfm75$fileDscr$datafile$B)[2], Inf)
 #
 #     rxml[46] <- "<range UNITS=\"INT\" max=\"-91\"/>"
 #     writeLines(rxml, file.path(tmp, "x.xml"))
-#     xdfm75 <- getMetadata(file.path(tmp, "x.xml"))
+#     xdfm75 <- getCodebook(file.path(tmp, "x.xml"))
 #     expect_equal(declared::missing_range(xdfm75$fileDscr$datafile$B)[1], -Inf)
 #
 #     rxml[46] <- "<range UNITS=\"INT\" min=\"-99\" max=\"-91\"/>"
@@ -92,13 +92,13 @@ test_that("an error is triggered if the XML file is not valid", {
 #     xdfm$dataDscr$var$C$measurement <- "unobserved"
 #     xdfm$dataDscr$var$D$measurement <- "ratio"
 #     xdfm$dataDscr$var$E$txt <- "This is just a normal character variable"
-#     exportDDI(xdfm, file = file.path(tmp, "x.xml"))
-#     xxml <- getMetadata(file.path(tmp, "x.xml"))
+#     exportCodebook(xdfm, file = file.path(tmp, "x.xml"))
+#     xxml <- getCodebook(file.path(tmp, "x.xml"))
 #     expect_true(is.list(xxml))
 # })
 
 # test_that("an error is triggered if the embedded CSV file is not valid", {
 #     rxml[43] <- "\"A\"	\"B\"	\"C\"	\"D\"	\"E\""
 #     writeLines(rxml, file.path(tmp, "x.xml"))
-#     expect_error(getMetadata(file.path(tmp, "x.xml")))
+#     expect_error(getCodebook(file.path(tmp, "x.xml")))
 # })
